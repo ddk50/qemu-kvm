ac32d2102d14383c05edcd6a1e24eafc374c8cca
ddk <ddk@ddk-ThinkPad-X60.(none)>
2011-06-07 15:13:21 +0900

    add necessary files

---
+++ sha1.h
@@ -0,0 +1,25 @@
+
+#ifndef _SHA1_H_
+#define _SHA1_H_
+
+/* ================ sha1.h ================ */
+/*
+SHA-1 in C
+By Steve Reid <steve@edmweb.com>
+100% Public Domain
+*/
+
+typedef struct {
+    uint32_t state[5];
+    uint32_t count[2];
+    unsigned char buffer[64];
+} SHA1_CTX;
+
+void SHA1Transform(uint32_t state[5], const unsigned char buffer[64]);
+void SHA1Init(SHA1_CTX* context);
+void SHA1Update(SHA1_CTX* context, const unsigned char* data, uint32_t len);
+void SHA1Final(unsigned char digest[20], SHA1_CTX* context);
+int compsha1(unsigned char *buf1, unsigned char *buf2, int buf_size);
+/* ================ end of sha1.h ================ */
+
+#endif
